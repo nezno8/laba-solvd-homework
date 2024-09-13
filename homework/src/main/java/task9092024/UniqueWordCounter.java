6122e7996e7cca5e2e78a4fa71dcc6101abe37e5
Nezno8 <zenitechq@gmail.com>
2024-09-13 21:01:57 +0200

    Separating resources for testing, correcting typos

--- homework/src/main/java/task9092024/UniqueWordCounter.java
+++ homework/src/main/java/task9092024/UniqueWordCounter.java
@@ -3,17 +3,13 @@ package task9092024;
 import java.io.*;
 
 public class UniqueWordCounter {
-    public static String readFromFie(File inputFile) throws IOException {
-        StringBuilder contentOfFie = new StringBuilder();
-
+    public static String readFile(File inputFile) throws IOException {
+        StringBuilder contentOfFile = new StringBuilder();
         BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
-
         String line;
-
         while( (line = bufferedReader.readLine() ) != null){
-            contentOfFie.append(line).append("\n");
+            contentOfFile.append(line).append("\n");
         }
-
-        return contentOfFie.toString().trim();
+        return contentOfFile.toString().trim();
     }
 }
--- homework/src/test/java/task9092024/UniqueWordCounterTest.java
+++ homework/src/test/java/task9092024/UniqueWordCounterTest.java
@@ -1,21 +1,33 @@
 package task9092024;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UniqueWordCounterTest {
 
+    private static File inputFile;
+    @BeforeAll
+    public static void setUp() {
+        inputFile = new File("..\\homework\\src\\test\\resources\\test_input_testReadFromFile.txt");
+    }
+
     @Test
-    public void testReadFromFie() throws IOException {
-        File inputFile = new File("..\\homework\\src\\main\\resources\\test_input_testReadFromFie.txt");
-        String expectedOutput = "Ala have cat and cat have Ala";
-        String actualOutput = UniqueWordCounter.readFromFie(inputFile);
+    public void testReadFromFile() throws IOException {
+        String expectedOutput = "Ala has cat and cat has Ala\nTod has turtle and turtle has Tod";
+        String actualOutput = UniqueWordCounter.readFile(inputFile);
         assertEquals(expectedOutput, actualOutput);
     }
+
+//    @Test
+//    public void testSplitWordsInLine(){
+//        String [] expectedOutput = "Ala have cat and cat have Ala".split(" ");
+//        String [] actualOutput = UniqueWordCounter.splitWordsInLine(appendsLinesByNewLine);
+//        assertEquals(expectedOutput, actualOutput);
+//    }
 
 }
