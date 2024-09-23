13be501196590f072237f2b5d8be88a6d783c141
Nezno8 <zenitechq@gmail.com>
2024-09-23 16:50:56 +0200

    Create Threads using Callable

--- homework/src/main/java/task19092024/Main.java
+++ homework/src/main/java/task19092024/Main.java
@@ -1,11 +1,28 @@
 package task19092024;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Thread thread1 = new Thread(new MyRunnable(), "Thread-1");
         Thread thread2 = new Thread(new MyRunnable(), "Thread-2");
 
         thread1.start();
         thread2.start();
+
+        Callable<String> callableTask = () -> {
+            return "Hello from Callable!";
+        };
+
+        FutureTask<String> futureTask = new FutureTask<>(callableTask);
+        Thread callableThread = new Thread(futureTask);
+
+        callableThread.start();
+
+        String result = futureTask.get();
+
+        System.out.println(result);
+
     }
 }
